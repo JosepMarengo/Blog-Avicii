0732e6af83cbe6c4939db9dcfee267d3b593065b
JosepMarengo <168607895+JosepMarengo@users.noreply.github.com>
2024-10-11 19:45:51 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// let currentIndex = 0;
+// const slides = document.querySelectorAll('.carousel-item');
+
+// function showSlide(index) {
+//     const slidesContainer = document.querySelector('.carousel-inner');
+//     slidesContainer.style.transform = `translateX(${-index * 100}%)`;
+// }
+
+// function nextSlide() {
+//     currentIndex = (currentIndex + 1) % slides.length;
+//     showSlide(currentIndex);
+// }
+
+// function prevSlide() {
+//     currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+//     showSlide(currentIndex);
+// }
+
+// setInterval(nextSlide, 3000); // Cambia de imagen cada 3 segundos
